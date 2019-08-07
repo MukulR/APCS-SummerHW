71d9e15614301f28829332650ac18e902be8528e
mukulibukuli <mukulgrao@gmail.com>
2019-08-07 06:44:26 -0700

    implement sumsquares

---
+++ src/com/mukul/apcs/SumSquares.java
@@ -0,0 +1,20 @@
+package com.mukul.apcs;
+import java.util.Scanner;
+
+public class SumSquares {
+    public static int sumSquares(int start, int end) {
+        int total = 0;
+        for (int i = start; i <= end; i++){
+            total = total + i*i;
+        }
+        return total;
+    }
+
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter two integers");
+        int start = reader.nextInt();
+        int end = reader.nextInt();
+        System.out.println(sumSquares(start, end));
+    } //End of main
+} //End of SumSquares Class
